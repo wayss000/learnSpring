ffa178e149b6c6ce45b25247502e82116fcc53fb
wayss000 <wayss000@gmail.com>
2017-04-20 00:58:46 +0800

    提交ApplicationContext练习

---
+++ SpringApplicationContext/readme.md
@@ -0,0 +1,3 @@
+用ApplicationContext容器注入Bean.
+
+[Spring学习二：Spring管理Java Bean的容器介绍](http://blog.csdn.net/qq1332479771/article/details/70247928).
---
+++ SpringApplicationContext/src/main/java/pers/wayss/springApplicationContext/Main.java
@@ -0,0 +1,22 @@
+package pers.wayss.springApplicationContext;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        //两种创建方式
+        //绝对路径创建方式
+        ApplicationContext fileContext = new FileSystemXmlApplicationContext
+              ("E:/code/java-web/learnSpring/SpringApplicationContext/src/main/resourse/Beans.xml");
+        HelloWorld obj1 = (HelloWorld) fileContext.getBean("helloWorld");
+        obj1.getMessage();
+        
+        //相对路径创建方式
+        ApplicationContext classContext = new ClassPathXmlApplicationContext("classpath:Beans.xml");
+        //此处的helloWorld对应Beans.xml中配置的<bean>标签的id
+        HelloWorld obj2 = (HelloWorld) classContext.getBean("helloWorld");
+        obj2.getMessage();
+     }
+  }
