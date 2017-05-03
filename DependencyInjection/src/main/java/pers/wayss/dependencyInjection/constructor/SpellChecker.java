53f42b465b6180f3290e853c1e7244a1fcbe3f1a
wayss000 <wayss000@gmail.com>
2017-05-04 00:27:57 +0800

    提交构造函数注入

---
+++ DependencyInjection/src/main/java/pers/wayss/dependencyInjection/constructor/SpellChecker.java
@@ -0,0 +1,10 @@
+package pers.wayss.dependencyInjection.constructor;
+
+public class SpellChecker {
+    public SpellChecker(){
+       System.out.println("Inside SpellChecker constructor." );
+    }
+    public void checkSpelling() {
+       System.out.println("Inside checkSpelling." );
+    } 
+ }
