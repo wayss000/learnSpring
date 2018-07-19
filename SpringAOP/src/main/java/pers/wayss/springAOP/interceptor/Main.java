60358d3c656b117feb2e6c8ececd1d9be044262d
wayss000 <wayss000@gmail.com>
2018-07-20 01:31:58 +0800

    提交SpringAOP初试demo

---
+++ SpringAOP/src/main/java/pers/wayss/springAOP/interceptor/Main.java
@@ -0,0 +1,20 @@
+package pers.wayss.springAOP.interceptor;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans-config.xml");
+
+        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+        objA.getMessage1();
+        objA.getMessage2();
+
+        HelloInheritance objB = (HelloInheritance) context.getBean("helloInheritance");
+        objB.getMessage1();
+        objB.getMessage2();
+        objB.getMessage3();
+     }
+  }
---
+++ SpringAOP/src/main/java/pers/wayss/springAOP/interceptor/TestInterceptor.java
@@ -0,0 +1,10 @@
+package pers.wayss.springAOP.interceptor;
+
+/**
+ * Spring拦截器
+ * @author Wayss
+ * @date 2018/7/20
+ */
+
+public class TestInterceptor {
+}
